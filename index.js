6891e969a4dce2d3ab94e6f064f8e678b2a81c4e
Nedal1994 <nedal_al_saleh94@hotmail.com>
2025-09-29 15:13:06 +0300

    ok

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello devops");
+console.log("lening ci/cd in github actions");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("start test");
+setTimeout(() => {
+    console.log("waiting 3 seconds");
+    
+}, 3000);
+console.log("test complete");
